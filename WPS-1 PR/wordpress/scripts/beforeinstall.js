d2eeb53dffcb6bb4578cc4505516797cb478e99d
mohamedibrahim00 <65600414+mohamedibrahim00@users.noreply.github.com>
2020-09-29 17:36:11 +0530

    Update beforeinstall.js

--- WPS-1 PR/wordpress/scripts/beforeinstall.js
+++ WPS-1 PR/wordpress/scripts/beforeinstall.js
@@ -12,7 +12,7 @@ if (${settings.ls-addon:false}) {
     tag: "5.4.1-php-7.3.7",
     count: 1,
     flexibleCloudlets: ${settings.flexibleCloudlets:16},
-    fixedCloudlets: ${settings.fixedCloudlets:8},
+    fixedCloudlets: ${settings.fixedCloudlets:1},
     diskLimit: ${settings.diskLimit:10},
     nodeGroup: "cp",
     displayName: "AppServer",
